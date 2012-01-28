3da526fbf3db60a3aa4274443724370b7cc5d943
Saurabh Sood <saurabhsood91@gmail.com>
2012-01-28 17:24:49 +0530

    Added Comments to Calculator

--- calculate/calcmain.py
+++ calculate/calcmain.py
@@ -10,26 +10,26 @@ from PyQt4.QtGui import *
 from math import *
 
 class Form(QDialog):
-    def __init__(self,parent=None):
-        super(Form,self).__init__(parent)
-        self.browser=QTextBrowser()
-        self.lineEdit=QLineEdit("Enter Expression and Press Enter: ")
-        self.lineEdit.selectAll()
-        self.layout=QVBoxLayout()
-        self.layout.addWidget(self.browser)
-        self.layout.addWidget(self.lineEdit)
-        self.setLayout(self.layout)
-        self.lineEdit.setFocus()
-        self.connect(self.lineEdit, SIGNAL("returnPressed()"),self.updateUi)
-        self.setWindowTitle("Calculate")
-        self.show()
+    def __init__(self,parent=None):             #constructor with parent as none by default, signifying top level window
+        super(Form,self).__init__(parent)       #constructing the base class, which is QDialog
+        self.browser=QTextBrowser()             #creating a text browser
+        self.lineEdit=QLineEdit("Enter Expression and Press Enter: ")       #creating a line edit for entering expression
+        self.lineEdit.selectAll()                                           #selecting the whole text by default
+        self.layout=QVBoxLayout()                                           #using VBoxLayout
+        self.layout.addWidget(self.browser)                                 #adding browser to layout
+        self.layout.addWidget(self.lineEdit)                                #adding lineedit to layout
+        self.setLayout(self.layout)                                         #setting the main layout to the defined layout
+        self.lineEdit.setFocus()                                            #setting focus to lineedit
+        self.connect(self.lineEdit, SIGNAL("returnPressed()"),self.updateUi)    #connecting return pressed to updateUI slot
+        self.setWindowTitle("Calculate")                                        #setting window title to calculate
+        self.show()                                                             #showing the UI
     def updateUi(self):
         try:
-            text=unicode(self.lineEdit.text())
-            self.browser.append("%s = <b>%s</b>" % (text,eval(text)))
-            self.lineEdit.clear()
+            text=unicode(self.lineEdit.text())                                  #extracting the text in the lineedit
+            self.browser.append("%s = <b>%s</b>" % (text,eval(text)))           #evaluating the expression and showing in browser
+            self.lineEdit.clear()                                               #clearing the expression lineedit
         except:
-            self.browser.append("<font color=red>Invalid Expression</font> - %s" % (text))
+            self.browser.append("<font color=red>Invalid Expression</font> - %s" % (text))  #if invalid expression, show msg
 
 if __name__!="main":
     app=QApplication(sys.argv)
